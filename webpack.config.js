b4e7ebd36c0499a8f7eb3b24fabc181587f133eb
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2020-12-30 22:32:46 +0900

    setting environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,31 @@
-// * 
+// *
 const path = require('path');
 
 // * バンドルの起点を設定
 module.exports = {
   entry: {
-    bundle: './src/indes.ts'
+    bundle: './src/index.ts'
   },
   // *出力先を設定する
+  output: {
+    path: path.join(__dirname, 'dist'),
+    // * バンドルされたファイル
+    filename: '[name].js'
+  },
+  // *
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        loader: 'ts-loader',
+        test: /\.ts$/
+      }
+    ]
+  }
 }
